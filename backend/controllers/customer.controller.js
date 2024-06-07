618f8dcfb6bd66ea2d1e05754901c3f371599fea
Rohan <rohan.2021b0131077@abes.ac.in>
2024-06-07 22:12:12 +0530

    added controllers for customer and order, dynamic and customer depends on order for totalSpending

--- backend/controllers/customer.controller.js
+++ backend/controllers/customer.controller.js
@@ -2,6 +2,39 @@ import Customer from "../models/customer.model.js";
 import Order from "../models/order.model.js";
 import bcrypt from 'bcryptjs'
 export const createCustomer=async(req,res)=>{
-   
+    try{
+        const {fullName, userName, password, lastVisit, totalSpending, totalVisits } = req.body
+
+        const cust = await Customer.findOne({userName})
+
+        if(cust){
+            return res.status(400).json({error: "Customer with this username already exists"})
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(password, salt)
+
+        const newCust = new Customer({
+            fullName,
+             userName,
+             password: hashedPass,
+             lastVisit,
+             totalSpending,
+             totalVisits
+          
+        })
+
+        await newCust.save();
+
+        res.status(201).json({
+            data: newCust,
+            message: "Customer ingested successfully"
+        })
+
+    }catch(err){
+        res.status(500).json({error: `Error creating customer- ${err.message}`})
+    }
+
+
 
 }
--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -2,5 +2,29 @@ import Order from "../models/order.model.js";
 import Customer from "../models/customer.model.js";
 
 export const createOrder=async(req,res)=>{
+    
+    try{
+        const {orderingUser, amount , orderDate, orderType } = req.body;
 
+        
+        const user = await Customer.findOne({userName: orderingUser})
+        
+        const userId = user._id;
+        user.totalSpending += parseInt( amount);
+        user.totalVisits+=1;
+
+        const newOrder = new Order({
+            customerId: userId,
+            amount,
+            orderDate,
+            orderType
+        })
+
+        await Promise.all([newOrder.save(), user.save()])
+
+        res.status(201).json({message: "Order ingested successfully!"});
+
+        }catch(err){
+        res.status(500).json({error: `Error creating orders!- ${err.message}`})
+    }
 }
